0fc9ffdedd4bf2e8af33701431dd17dd24bc0c1b
Deivid-Martins <deividrocha2005@gmail.com>
2026-06-18 09:11:10 -0300

    feat: created initial response error json

--- src/CashFlow.Application/UseCases/Expenses/Register/RegisterExpenseUseCase.cs
+++ src/CashFlow.Application/UseCases/Expenses/Register/RegisterExpenseUseCase.cs
@@ -1,4 +1,5 @@
-﻿using CashFlow.Communication.Requests;
+﻿using CashFlow.Communication.Enums;
+using CashFlow.Communication.Requests;
 using CashFlow.Communication.Responses;
 
 namespace CashFlow.Application.UseCases.Expenses.Register;
@@ -7,8 +8,34 @@ public class RegisterExpenseUseCase
 {
     public ResponseRegisteredExpenseJson Execute(RequestRegisterExpenseJson request)
     {
-        // TO DO Validations
+        Validate(request);
 
         return new ResponseRegisteredExpenseJson();
+    }
+
+    private void Validate(RequestRegisterExpenseJson request)
+    {
+        var titleIsEmpty = string.IsNullOrWhiteSpace(request.Title);
+        if (titleIsEmpty)
+        {
+            throw new ArgumentException("Title is required.");
+        }
+
+        if(request.Amount <= 0)
+        {
+            throw new ArgumentException("Amount must be greater than zero.");
+        }
+
+        var dateIsValid = DateTime.Compare(request.Date, DateTime.UtcNow);
+        if (dateIsValid > 0)
+        {
+            throw new ArgumentException("Expense date cannot be in the future.");
+        }
+
+        var paymentTypeIsValid = Enum.IsDefined(typeof(PaymentType), request.PaymentType);
+        if(paymentTypeIsValid == false)
+        {
+            throw new ArgumentException("Invalid payment type.");
+        }
     }
 }
